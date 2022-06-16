548ff69077cbf0731516e40e5edc31116afff340
yeomboyeon <yeomboyeon1221@gmail.com>
2022-06-16 14:43:56 +0900

    ArratList 기능 구현, article list 코드 보완

---
+++ src/App/Article.java
@@ -0,0 +1,15 @@
+package App;
+
+public class Article {
+	int id;
+	String title;
+	String body;
+	
+	public Article(int id, String title, String body) {
+	this.id = id; // 인자를 불러오고 조정할 수 있도록 추가
+	this.title = title;
+	this.body = body;
+	
+	}
+
+}
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,45 +1,59 @@
 package Main;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import App.Article;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("===== 프로그램 시작 =====");
 
-		Scanner sc = new Scanner(System.in); // 스캐너 불러오는 함수
+		Scanner sc = new Scanner(System.in);
 		int lastArticleId = 0;
+		List<Article> articles = new ArrayList<>(); // 글생성시 보관하는 창고 역할 list 생성
 
-		while (true) { // 무조건 반복하는 반복문
+		while (true) {
 
-			System.out.printf("명령어) "); // 입력하기 위한 명령어 제목
+			System.out.printf("명령어) ");
 
-			String command = sc.nextLine(); // 입력하는 부분
-
+			String command = sc.nextLine().trim(); // 빈칸 적용을 없애주는 함수 추가
+			
 			if (command.length() == 0) {
 				System.out.println("명령어를 입력해 주세요.");
-				continue; // if 문 바로 위로 빠져나가는 함수
-				// break; 반복문을 완전 빠져나가는 함수
+				continue;
 			}
-			if (command.equals("system exit")) { // command 변수 입력시 프로그램 종료되도록 작성
+			if (command.equals("system exit")) {
 				break;
 			}
-			if (command.equals("article write")) { // 글 작성시 글번호가 생성되어야 하기에 lastArticleId를 int로 정의
-				int id = lastArticleId + 1; // 생성하는 글 번호는 마지막에 작성된 글 마지막 번호 +1이 되어야 생성
+			if (command.equals("article write")) {
+								
+				int id = lastArticleId + 1;
 				lastArticleId = id;
 				System.out.printf("제목 : ");
-				String title = sc.nextLine(); // 입력하는 부분
+				String title = sc.nextLine();
 				System.out.printf("내용 : ");
-				String body = sc.nextLine(); // 입력하는 부분
-
+				String body = sc.nextLine();
+				
+				Article article = new Article(id,title,body); // 글 생성시 article 을 생성하고 인자 3가지를 불러온다.
+				articles.add(article); //article에 추가한다.
+				
 				System.out.printf(id + "번 글이 생성되었습니다.\n");
-			} else if (command.equals("article list")) { 
-				System.out.println("게시글이 없습니다.");
+				
+			} else if (command.equals("article list")) {
+				if(articles.size() == 0) { // 글이 저장되는 articles에 글이 있다 없다에 따라 게시글이 있다 없다를 표현
+					System.out.println("게시글이 없습니다.");
+					continue;
+				}else {
+					System.out.println("게시글이 있습니다.");
+				}
+				
 			} else {
 				System.out.println("존재하지 않는 명령어입니다.");
 			}
 		}
-		sc.close(); // 스캐너 구문 종료 버튼
-
+		sc.close();
 		System.out.println("===== 프로그램 종료 =====");
 	}
 }
